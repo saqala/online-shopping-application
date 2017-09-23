ce67a8a95a9ffca25c57204f663ccede61fef886
JaLover <JaLover@protonmail.com>
2017-09-23 22:22:29 +0100

    Fix some Errors

--- onlineshoppingapplicationbackend/databaseQueries.sql
+++ onlineshoppingapplicationbackend/databaseQueries.sql
@@ -10,3 +10,7 @@ CREATE TABLE CATEGORY (
 
 
 );
+
+INSERT INTO CATEGORY (name, description, image_url, is_active) VALUES('Phone', 'Description of phones here', 'Phone.jpg', true)
+INSERT INTO CATEGORY (name, description, image_url, is_active) VALUES('TV', 'Description of TV', 'tvs.jpg', true)
+INSERT INTO CATEGORY (name, description, image_url, is_active) VALUES('Laptop', 'Description of Laptops', 'laptop.jpg', true)
