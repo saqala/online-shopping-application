5384026504ed016ee9474e29d78e1f8eaad2c315
JaLover <JaLover@protonmail.com>
2017-10-01 01:53:14 +0100

    product Server Validation.

--- onlineshoppingapplication/src/main/java/com/example/onlineshoppingapplication/controller/ManagementController.java
+++ onlineshoppingapplication/src/main/java/com/example/onlineshoppingapplication/controller/ManagementController.java
@@ -2,10 +2,14 @@ package com.example.onlineshoppingapplication.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,16 +25,16 @@ import example.com.onlineshoppingapplicationbackend.dto.Product;
 @RequestMapping("/manage")
 public class ManagementController {
 
-	
 	@Autowired
 	private CategoryDao categoryDao;
-	
+
 	@Autowired
 	private ProductDao productao;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ManagementController.class);
+
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
-	public ModelAndView showManageProducts(@RequestParam(name="operation", required=false) String operation) {
+	public ModelAndView showManageProducts(@RequestParam(name = "operation", required = false) String operation) {
 
 		ModelAndView mv = new ModelAndView("page");
 
@@ -38,35 +42,41 @@ public class ManagementController {
 
 		mv.addObject("title", "Manage Products");
 
-		
 		Product nProduct = new Product();
-		
+
 		nProduct.setSupplierId(1);
 		nProduct.setActive(true);
-		
+
 		mv.addObject("product", nProduct);
-		
-		if(operation != null) {
-			if(operation.equals("product")) {
+
+		if (operation != null) {
+			if (operation.equals("product")) {
 				mv.addObject("message", "Product Submitted Successfully");
 			}
 		}
 		return mv;
 
 	}
-	
+
 	@ModelAttribute("categories")
 	public List<Category> getCategories() {
-		
+
 		return categoryDao.listCategory();
 	}
-	
-	@RequestMapping(value="/products", method=RequestMethod.POST)
-	public String handleProductSubmission(@ModelAttribute("product") Product mProduct) {
-		
+
+	@RequestMapping(value = "/products", method = RequestMethod.POST)
+	public String handleProductSubmission(@Valid @ModelAttribute("product") Product mProduct, BindingResult result,
+			Model model) {
+
+		if(result.hasErrors()) {
+			model.addAttribute("userClickManageProducts", true);
+			model.addAttribute("title", "Manage Products");
+			model.addAttribute("message", "Validation failed for Product Submission!");
+			return "page";
+		}
 		logger.info(mProduct.toString());
 		productao.add(mProduct);
-		
+
 		return "redirect:/manage/products?operation=product";
 	}
 }
--- onlineshoppingapplicationbackend/src/main/java/example/com/onlineshoppingapplicationbackend/dto/Product.java
+++ onlineshoppingapplicationbackend/src/main/java/example/com/onlineshoppingapplicationbackend/dto/Product.java
@@ -9,7 +9,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,19 +23,20 @@ public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	
 	// private fields
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String code;
-	
+	@NotBlank(message = "Please enter the product name!")
 	private String name;
-	
+	@NotBlank(message = "Please enter the brand name!")
 	private String brand;
-	@JsonIgnore
+	@NotBlank(message = "Please enter the description!")
 	private String description;
 	@Column(name = "unit_price")
-	
+	@Min(value = 1, message="Please select at least one value!")
 	private double unitPrice;
 	private int quantity;
 	@Column(name = "is_active")	
@@ -45,8 +50,18 @@ public class Product implements Serializable {
 	private int purchases;
 	private int views;
 	
-	
 
+	@Transient
+	private MultipartFile file;
+
+	public MultipartFile getFile() {
+		return file;
+	}
+
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
 
 
 	// default constructor
