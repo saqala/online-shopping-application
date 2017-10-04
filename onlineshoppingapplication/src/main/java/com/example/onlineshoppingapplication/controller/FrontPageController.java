734931edae81db7be8a13b5df152260519a7c537
JaLover <JaLover@protonmail.com>
2017-10-04 22:19:35 +0100

    Adding Logout page

--- onlineshoppingapplication/src/main/java/com/example/onlineshoppingapplication/controller/FrontPageController.java
+++ onlineshoppingapplication/src/main/java/com/example/onlineshoppingapplication/controller/FrontPageController.java
@@ -1,8 +1,14 @@
 package com.example.onlineshoppingapplication.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -108,6 +114,17 @@ public class FrontPageController {
 		return mv;
 	}
 	
+	@RequestMapping(value="/logout")
+	public String logout(HttpServletRequest request, HttpServletResponse response) {
+		// Invalidates HTTP Session, then unbinds any objects bound to it.
+	    // Removes the authentication from securitycontext 		
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+		
+		return "redirect:/login?logout";
+	}	
 	@RequestMapping(value="/login")
 	public ModelAndView login(@RequestParam(name="error", required = false)	String error,
 			@RequestParam(name="logout", required = false) String logout) {
