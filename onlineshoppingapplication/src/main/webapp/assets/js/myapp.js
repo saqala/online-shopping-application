289ce7a7f275117cd3d888807272e212aed61d52
JaLover <JaLover@protonmail.com>
2017-10-04 19:45:56 +0100

    Handling CSRF using Spring Security

--- onlineshoppingapplication/src/main/webapp/assets/js/myapp.js
+++ onlineshoppingapplication/src/main/webapp/assets/js/myapp.js
@@ -28,6 +28,17 @@ $(function() {
 	}
 	
 	
+	var token = $('meta[name="_csrf"]').attr('content');
+	var header = $('meta[name="_csrf_header"]').attr('content');
+	
+	if((token!=undefined && header !=undefined) && (token.length > 0 && header.length > 0)) {		
+		// set the token header for the ajax request
+		$(document).ajaxSend(function(e, xhr, options) {			
+			xhr.setRequestHeader(header,token);			
+		});				
+	}
+	
+	
 	// code for jquery database
 	
  var jsonUrl = '';
