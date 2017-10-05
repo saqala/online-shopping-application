cb0680da247719d2f2cc0d022b4b8b93ae7addbd
JaLover <JaLover@protonmail.com>
2017-10-05 06:53:53 +0100

    Adding Heroku support

--- onlineshoppingapplicationbackend/src/main/java/example/com/onlineshoppingapplicationbackend/config/HibernateConfig.java
+++ onlineshoppingapplicationbackend/src/main/java/example/com/onlineshoppingapplicationbackend/config/HibernateConfig.java
@@ -18,25 +18,28 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class HibernateConfig {
 
-	private final static String DATABASE_URL = "jdbc:h2:tcp://localhost/~/onlineshopping";
-	private final static String DATABASE_DRIVER = "org.h2.Driver";
+/*	private final static String DATABASE_URL = "postgres://sgxuzxwstkiiip:c48eb89c77a06613e097f6f192353c59de50d8a2230b9d0dc297232c6a4dbfa0@ec2-107-22-235-167.compute-1.amazonaws.com:5432/d3doq2hifk108v";
+	private final static String DATABASE_DRIVER = "org.postgresql.Driver";
 	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
-	private final static String DATABASE_USERNAME = "sa";
-	private final static String DATABASE_PASSWORD = "";
-
+	private final static String DATABASE_USERNAME = "sgxuzxwstkiiip";
+	private final static String DATABASE_PASSWORD = "c48eb89c77a06613e097f6f192353c59de50d8a2230b9d0dc297232c6a4dbfa0";
+*/
+	private final static String DATABASE_DIALECT = "org.hibernate.dialect.H2Dialect";
 	@Bean("dataSource")
 	public DataSource getDataSource() {
 
-		BasicDataSource dataSource = new BasicDataSource();
+		/*BasicDataSource dataSource = new BasicDataSource();*/
 
-		// Provide The database's connection information
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
 
-		dataSource.setDriverClassName(DATABASE_DRIVER);
-		dataSource.setUrl(DATABASE_URL);
-		dataSource.setUsername(DATABASE_USERNAME);
-		dataSource.setPassword(DATABASE_PASSWORD);
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
 		
-		return dataSource;
+		return basicDataSource;
 
 	}
 	
